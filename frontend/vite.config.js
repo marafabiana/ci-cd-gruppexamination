50762b0043e877eeb4f8951bd4a11a8b6d4bfa57
Mara Benevides <marafabianalb@gmail.com>
2024-10-31 17:10:27 +0100

    Add dist folder to .gitignore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 5173
-  }
- 
-})
+  base: process.env.NODE_ENV === "production" ? "/ci-cd-gruppexamination/" : "/",
+});
