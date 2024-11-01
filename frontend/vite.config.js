98389c5581cd4af33d580c7887f0f4e02dc02a0f
Mara Benevides <marafabianalb@gmail.com>
2024-11-01 17:14:14 +0100

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ci-cd-gruppexamination/frontend',
-})
+  base: "/ci-cd-gruppexamination",
+});
