fa68f37ead146b40c3d88675023fdaccc0febb24
Khaled <kh.almelhem@gmail.com>
2024-11-01 18:37:31 +0100

    fixing bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import EventsPage from './pages/EventsPage'; 
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import EventsPage from "./pages/EventsPage";
 
 const App = () => {
-  const isLoggedIn = Boolean(localStorage.getItem('token'));
+  const isLoggedIn = Boolean(localStorage.getItem("token"));
 
   return (
     <>
@@ -17,8 +17,14 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={isLoggedIn ? <Profile /> : <Navigate to="/login" />} />
-        <Route path="/events" element={isLoggedIn ? <EventsPage /> : <Navigate to="/login" />} />
+        <Route
+          path="/profile"
+          element={isLoggedIn ? <Profile /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/events"
+          element={isLoggedIn ? <EventsPage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </>
   );
